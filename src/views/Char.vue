a20b990e495fcced13026af0d8644d30b57e02d7
George Soteriou <george@soteriou.me>
2021-06-25 19:11:48 +0100

    testing dragging

--- src/views/Char.vue
+++ src/views/Char.vue
@@ -1,17 +1,17 @@
 <template>
-  <v-container fluid>
-    <v-row class="mb-10">
-      <v-col cols="12" lg="12" xl="12"><Info /></v-col>
-      <v-col cols="12" lg="6" xl="3"><Stats /></v-col>
-      <v-col cols="12" lg="6" xl="3"><Health /></v-col>
-      <v-col cols="12" lg="6" xl="3"><AbilityMods /></v-col>
-      <v-col cols="12" lg="6" xl="3"><SavingThrows /></v-col>
-      <v-col cols="12" lg="6" xl="3"><Skills /></v-col>
-      <v-col cols="12" lg="6" xl="3"><Armor /></v-col>
-      <v-col cols="12" lg="6" xl="3"><Equipment /></v-col>
-      <v-col cols="12" lg="6" xl="3"><Weapons /></v-col>
-    </v-row>
-  </v-container>
+  <draggable v-model="order" class="container container--fluid">
+    <transition-group class="row mb-10">
+      <v-col draggable :key="1" cols="12" lg="12" xl="12"><Info /></v-col>
+      <v-col draggable :key="2" cols="12" lg="6" xl="3"><Stats /></v-col>
+      <v-col draggable :key="3" cols="12" lg="6" xl="3"><Health /></v-col>
+      <v-col draggable :key="4" cols="12" lg="6" xl="3"><AbilityMods /></v-col>
+      <v-col draggable :key="5" cols="12" lg="6" xl="3"><SavingThrows /></v-col>
+      <v-col draggable :key="6" cols="12" lg="6" xl="3"><Skills /></v-col>
+      <v-col draggable :key="7" cols="12" lg="6" xl="3"><Armor /></v-col>
+      <v-col draggable :key="8" cols="12" lg="6" xl="3"><Equipment /></v-col>
+      <v-col draggable :key="9" cols="12" lg="6" xl="3"><Weapons /></v-col>
+    </transition-group>
+  </draggable>
 </template>
 
 <script>
@@ -25,9 +25,17 @@ import Armor from "../components/Armor.vue";
 import Equipment from "../components/Equipment.vue";
 import Weapons from "../components/Weapons.vue";
 
+import draggable from "vuedraggable";
+
 export default {
   name: "Char",
+  data: function () {
+    return {
+      order: null,
+    };
+  },
   components: {
+    draggable,
     Info,
     Health,
     Stats,
