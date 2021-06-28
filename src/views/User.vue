4491df410dc129a5d4696366a00487bb06ce79f7
George Soteriou <george@soteriou.me>
2021-06-28 16:20:04 +0100

    add char delete

--- src/views/User.vue
+++ src/views/User.vue
@@ -3,9 +3,29 @@
     <v-row class="text-center text-h2" justify="center">
       <v-col cols="12"> Character Sellection </v-col>
       <v-col cols="10" :key="i" v-for="(char, i) in chars">
-        <v-btn color="blue" block x-large :href="`char/${char.id}`">
-          {{ char.name }}
-        </v-btn>
+        <v-row dense>
+          <v-col cols="10">
+            <v-btn
+              color="blue"
+              block
+              x-large
+              class="pr-0"
+              :href="`char/${char.id}`"
+            >
+              {{ char.name }}
+            </v-btn>
+          </v-col>
+          <v-col cols="2">
+            <v-btn
+              block
+              color="red"
+              x-large
+              @click="prepDel(char.id, char.name)"
+            >
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-col>
       <v-col cols="10">
         <v-btn color="success" block @click="newChar" x-large>
@@ -13,6 +33,22 @@
         </v-btn>
       </v-col>
     </v-row>
+    <v-dialog v-model="dialog" width="500">
+      <v-card class="pa-3">
+        <v-card-title class="text-h5 text-center">
+          Delete? Really?
+        </v-card-title>
+        <v-card-text class="text-center text-h4">
+          Are you sure you want to delete {{ prepForDel.name }}?
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="success" @click="deleteChar()"> Yes, Delete </v-btn>
+          <v-btn color="error" @click="dialog = false"> No! Go back </v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <v-footer absolute color="primary" padless>
       <v-row justify="center" no-gutters>
         <v-btn text block dark x-large @click="logout"> Logout </v-btn>
@@ -29,7 +65,9 @@ export default {
   components: {},
   data() {
     return {
+      dialog: false,
       chars: {},
+      prepForDel: {},
     };
   },
   firestore() {
@@ -49,6 +87,14 @@ export default {
     logout() {
       this.$store.commit("logout");
       this.$router.push("/");
+    },
+    prepDel(id, name) {
+      this.prepForDel = { id: id, name: name };
+      this.dialog = true;
+    },
+    deleteChar() {
+      db.collection("characters").doc(this.prepForDel.id).delete();
+      this.dialog = false;
     },
   },
 };
