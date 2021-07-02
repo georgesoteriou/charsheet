869de750edffd3ebb1bd6446b35cf9416aaa9aa8
George Soteriou <george@soteriou.me>
2021-07-02 12:05:58 +0100

    test online wisdom

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,13 @@ module.exports = {
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
+    manifestPath: 'manifest.json',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+        // swSrc is required in InjectManifest mode.
+        swSrc: 'service-worker.js',
+        // ...other Workbox options...
+        exclude: [/_redirects/],
+    }
   }
 }
